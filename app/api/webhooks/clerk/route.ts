c09d27439f47306979c82c249c2e7baabd4ceb92
eugerr <gardensensei4.com>
2024-08-19 20:18:29 +0800

    added createUser to webhook

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,6 +1,8 @@
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
 import { WebhookEvent } from '@clerk/nextjs/server'
+import { createUser } from '@/lib/users'
+import { User } from '@prisma/client'
 
 export async function POST(req: Request) {
   const WEBHOOK_SECRET = process.env.CLERK_WEBHOOK_SECRET
@@ -66,7 +68,7 @@ export async function POST(req: Request) {
       ...(image_url ? { imageUrl: image_url } : {}),
     }
     console.log(user)
-    // await createUser(user as User)
+    await createUser(user as User)
   }
 
   return new Response('working', { status: 200 })
